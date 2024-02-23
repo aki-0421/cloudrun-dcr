d0415829cbf7f6b9c2602c44aa55e04491a3833c
aki-0421 <118268728+aki-0421@users.noreply.github.com>
2024-02-23 12:12:52 +0900

    R: fix

--- main.go
+++ main.go
@@ -9,21 +9,9 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := struct {
-			Scheme   string `json:"scheme"`
-			Opaque   string `json:"opaque"`
-			User     string `json:"user"`
-			Host     string `json:"host"`
-			Path     string `json:"path"`
-			RawQuery string `json:"raw_query"`
-			Fragment string `json:"fragment"`
+			Host string `json:"host"`
 		}{
-			Scheme:   r.URL.Scheme,
-			Opaque:   r.URL.Opaque,
-			User:     r.URL.User.String(),
-			Host:     r.URL.Host,
-			Path:     r.URL.Path,
-			RawQuery: r.URL.RawQuery,
-			Fragment: r.URL.Fragment,
+			Host: r.Host,
 		}
 
 		w.Header().Set("Content-Type", "application/json")
