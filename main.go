7a8717270a897a8f762cfb9ee97a856cca0a1903
aki-0421 <118268728+aki-0421@users.noreply.github.com>
2024-02-23 11:56:05 +0900

    R: update app

--- main.go
+++ main.go
@@ -8,9 +8,26 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+		data := struct {
+			Scheme   string `json:"scheme"`
+			Opaque   string `json:"opaque"`
+			User     string `json:"user"`
+			Host     string `json:"host"`
+			Path     string `json:"path"`
+			RawQuery string `json:"raw_query"`
+			Fragment string `json:"fragment"`
+		}{
+			Scheme:   r.URL.Scheme,
+			Opaque:   r.URL.Opaque,
+			User:     r.URL.User.String(),
+			Host:     r.URL.Host,
+			Path:     r.URL.Path,
+			RawQuery: r.URL.RawQuery,
+			Fragment: r.URL.Fragment,
+		}
 
-		if err := json.NewEncoder(w).Encode(r.URL); err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(data); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
